7a3abac3870a213ab753f4b9331f5ac259c0001c
Antoine du Hamel <duhamelantoine1995@gmail.com>
2020-09-14 11:45:34 +0200

    Footer: add terms and bank info

--- src/Footer.js
+++ src/Footer.js
@@ -2,9 +2,25 @@ import { html } from "htm/preact";
 
 import STRINGS from "lang:strings.toml";
 
+const createTerm = (terms) =>
+  html`<section>
+    ${terms.HEADING ? html`<h5>${terms.HEADING}</h5>` : null}
+    <ul>
+      ${terms.TEXT.map((txt) => html`<li>${txt}</li>`)}
+    </ul>
+  </section>`;
+
 export default function Footer(props) {
   const { format } = props;
   return html`<footer>
+    ${STRINGS.TERMS.map(createTerm)}
+    <section>
+      <h5>${STRINGS.BANK_INFO.HEADING}</h5>
+      <ul>
+        <li>${STRINGS.BANK_INFO.IBAN} ${props.biller.IBAN}</li>
+        <li>${STRINGS.BANK_INFO.BIC} ${props.biller.BIC}</li>
+      </ul>
+    </section>
     <section>
       <h5>${STRINGS.SUBTOTAL}</h5>
       <output>${format.currency(props.subtotal)}</output>
