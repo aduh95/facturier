46afc3f214f3a00e0c10e141ee55a127e78e271f
Antoine du Hamel <duhamelantoine1995@gmail.com>
2020-09-14 12:01:31 +0200

    build: add support for SASS files

--- scripts/runtime.js
+++ scripts/runtime.js
@@ -2,10 +2,10 @@ import { fileURLToPath } from "url";
 import { rollup } from "rollup";
 
 import resolve from "@rollup/plugin-node-resolve";
-// import sass from "./rollup-plugin-sass.mjs";
+import sass from "./rollup-plugin-sass.mjs";
 import toml from "./rollup-plugin-toml.mjs";
 
-const plugins = [resolve(), /*sass(),*/ toml()];
+const plugins = [resolve(), sass(), toml()];
 
 const INPUT_DIR = new URL("../src/", import.meta.url);
 
