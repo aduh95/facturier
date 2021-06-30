25fd0bf7a0c797898a52474e9298822ec1b431e0
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-06-30 22:47:11 +0200

    Workaround Chromium bug when `<table>` goes multipages

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import "./index.scss";
 import BILLER_INFO from "../user_data/biller.toml";
 import INVOICE_INFO from "cli:argv[2].toml";
 
+if (INVOICE_INFO.line.length > 7) {
+  // Hack to workaround bug in chrome when table span into several pages while inside a flex container.
+  document.body.style.display = "revert";
+}
+
 render(
   html`<${Invoice} biller=${BILLER_INFO} ...${INVOICE_INFO} />`,
   document.body
