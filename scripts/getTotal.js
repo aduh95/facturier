dd4c3448bf6557cb52c0c59a5fdb259e697c4102
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-06-30 21:31:01 +0200

    Display more info from `getTotal` for invoice using hourly rate

--- scripts/getTotal.js
+++ scripts/getTotal.js
@@ -4,7 +4,7 @@ import fs from "fs";
 import TOML from "@aduh95/toml";
 import { getInvoiceFilePath } from "./get-invoice-info.js";
 
-const { client, currency, line, prepaid, tax } = TOML.parse(
+const { client, currency, line, prepaid, tax, hourlyRate } = TOML.parse(
   fs.readFileSync(getInvoiceFilePath())
 );
 
@@ -16,3 +16,28 @@ const total = line?.length
 
 console.log("Total without tax", total, currency);
 console.log("Balance incl. tax", total * (1 - tax) - prepaid, currency);
+
+if (hourlyRate) {
+  const { rate, nbOfDaysOff, targetedNbOfWorkHourPerDay } = hourlyRate;
+  const nbOfHours = total / rate;
+  console.log("\nIt looks like this invoice has an hourly rate:");
+  console.log("Number of hours: ", nbOfHours);
+  console.log(
+    "Assuming you are sending invoice at monthly basis, working 5 days a week:"
+  );
+
+  const now = new Date();
+  let nbOfWorkDay = -nbOfDaysOff;
+  for (let i = 1; i <= now.getDate(); i++) {
+    if (new Date(now.getUTCFullYear(), now.getMonth(), i).getDay() % 6) {
+      nbOfWorkDay++;
+    }
+  }
+  if (nbOfDaysOff !== 0) {
+    console.log("Number of work hours per week", (nbOfHours / nbOfWorkDay) * 5);
+    console.log(
+      "Number of work hours you need to work to meet the target",
+      targetedNbOfWorkHourPerDay * nbOfWorkDay - nbOfHours
+    );
+  }
+}
